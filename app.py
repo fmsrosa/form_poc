3ef66418783907af67365a14240c58c537b9ddfa
Fátima Rosa <fatimamargarida_santos@hotmail.com>
2025-03-24 23:46:49 +0000

    Fixed map

--- app.py
+++ app.py
@@ -20,33 +20,21 @@ option = st.selectbox(
 )
 
 
-# map
-col1, col2 = st.columns([0.8, 0.2])
-with col1:
-    m = folium.Map(location=[37.7749, -122.4194], zoom_start=10)
-    folium.ClickForLatLng(format_str='"LATLNG" + lat + "," + lng', alert=False).add_to(m)
-    folium.LatLngPopup().add_to(m)
-    st.components.v1.html(folium.Figure().add_child(m).render(), height=400)
-with col2:
-    st.markdown('''
-        Click on the map. Once you are satisfied, click on add coordinates.
-        More text
-        More text
-        More text
-        More text
-        ''')
-    st.button('Add coordinates', on_click=click_button)
+# map   
+import folium as fl
+from streamlit_folium import st_folium
+import streamlit as st
 
-    if st.session_state.clicked:
+m = fl.Map()
 
-        pasted = clipboard.paste()
-        st.session_state.clicked = False
-        if "LATLNG" in pasted:
-            st.session_state.coordinates = pasted[6:]
-        else:
-            st.session_state.coordinates = ""
-            st.write("Click on the map first")
-    if st.session_state.coordinates:
-        st.write(st.session_state.coordinates)
+m.add_child(fl.LatLngPopup())
+
+maps = st_folium(m, height=350, width=700)
+
+if maps['last_clicked']:
+    lat = maps['last_clicked']['lat']
+    lng = maps['last_clicked']['lng']
+    st.write(f'{lat}, {lng}')
 
 submitted = st.button("Submit")
+
